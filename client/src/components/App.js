6682fa4ad79b655377f5d6cad9cad75739205bde
dposse <dposse.125@gmail.com>
2019-08-08 21:22:26 -0400

    Restructured folders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import socket from './socket/socket';
+import socket from '../socket/socket';
 
 
 
--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
+const io = require('socket.io-client');
 const SOCKET_PORT = 5000;
 
-const io = require('socket.io-client');
 const socket = io(`http://localhost:${SOCKET_PORT}`);
 
 export default socket;
