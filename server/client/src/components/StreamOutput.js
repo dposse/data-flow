c7d03b95c682014e98eb1e176903c95d3f466193
dposse <dposse.125@gmail.com>
2019-08-26 11:44:40 -0400

    Added output component

--- server/client/src/components/StreamOutput.js
+++ server/client/src/components/StreamOutput.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
-const StreamOutput = () => {
+const StreamOutput = ({ board, lastMove }) => {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    const newInfo = info.concat([{ 
+      nextRow: board[1], 
+      move: lastMove, 
+      tick: (info.length > 0) ? info[info.length-1].tick+1 : 0 }])
+    if (newInfo.length > 10) {
+      newInfo.shift();
+    }
+    setInfo(newInfo);
+  }, [board, lastMove]);
+
   return (
     <StyledContainer>
-      streamoutput
+      {info.map((row) => {
+        return (
+          <OutputRow><span style={{float: 'left', width: '40px'}}>{row.tick}: </span>Next row: {row.nextRow}, Move: {row.move}</OutputRow>
+        );
+      })}
     </StyledContainer>
   )
 };
@@ -15,6 +33,22 @@ const StyledContainer = styled.div`
   border-radius: .2rem;
   margin-top: 20px;
   padding: 5px;
+  height: 200px;
+  width: 350px;
 `;
 
-export default StreamOutput;
+const OutputRow = styled.p`
+  margin: 0;
+  padding: 0;
+  padding-left: 5px;
+  font-size: .8em;
+`;
+
+function mapStateToProps(state) {
+  return {
+    board: state.board,
+    lastMove: state.lastMove
+  }
+}
+
+export default connect(mapStateToProps, null)(StreamOutput);
--- server/client/src/reducers/reducer-last-move.js
+++ server/client/src/reducers/reducer-last-move.js
@@ -1,6 +1,6 @@
 import { UPDATE_LAST_MOVE } from '../actions';
 
-export default function (state = '', action) {
+export default function (state = 'none', action) {
   if (action.error) return action.error;
   switch (action.type) {
     case UPDATE_LAST_MOVE:
