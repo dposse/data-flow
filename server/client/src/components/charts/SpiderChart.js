b4e863bb4476327899ffea02fed87d2c8bd72da7
dposse <dposse.125@gmail.com>
2019-09-09 19:56:55 -0400

    Refactored SpiderChart.js to use react hooks

--- server/client/src/components/charts/SpiderChart.js
+++ server/client/src/components/charts/SpiderChart.js
@@ -1,126 +1,111 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
 import { connect } from 'react-redux';
 
 require('highcharts/highcharts-more')(Highcharts);
 
-class SpiderChart extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      chartOptions: {
-        credits: {
-          enabled: false
-        },
-        chart: {
-          polar: true,
-          animation: false
-        },
-        title: {
-          text: 'Individual Bot Performance'
-        },
-        // pane: {
-        //   startAngle: 0,
-        //   endAngle: 360
-        // },
-        xAxis: {
-          categories: ['Steps', 'Movement right', 'Tiles Seen', 'Movement Left'],
-          lineWidth: 0
-        },
-        yAxis: {
-          gridLineInterpolation: 'polygon',
-          lineWidth: 0,
-          min: 0
-        },
-        legend: {
-          align: 'left',
-          verticalAlign: 'top'
-        },
-        plotOptions: {
-          series: {
-            animation: {
-              duration: 100
-            }
+const SpiderChart = ({ botStats }) => {
+  const [chartOptions, setChartOptions] = useState(
+    {
+      credits: {
+        enabled: false
+      },
+      chart: {
+        polar: true,
+        animation: false
+      },
+      title: {
+        text: 'Individual Bot Performance'
+      },
+      // pane: {
+      //   startAngle: 0,
+      //   endAngle: 360
+      // },
+      xAxis: {
+        categories: ['Steps', 'Movement right', 'Tiles Seen', 'Movement Left'],
+        lineWidth: 0
+      },
+      yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0
+      },
+      legend: {
+        align: 'left',
+        verticalAlign: 'top'
+      },
+      plotOptions: {
+        series: {
+          animation: {
+            duration: 100
           }
+        }
+      },
+      series: [
+        {
+          name: 'Random Bot',
+          data: [],
+          pointPlacement: 'on'
         },
-        series: [
-          {
-            name: 'Random Bot',
-            data: [],
-            pointPlacement: 'on'
-          },
-          {
-            name: 'Machine Learning Bot 1',
-            data: [],
-            pointPlacement: 'on'
-          },
-          {
-            name: 'Machine Learning Bot 2',
-            data: [],
-            pointPlacement: 'on'
-          }
-        ]
-      }
-    };
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props !== prevProps) {
-      this.updateSeries();
+        {
+          name: 'Machine Learning Bot 1',
+          data: [],
+          pointPlacement: 'on'
+        },
+        {
+          name: 'Machine Learning Bot 2',
+          data: [],
+          pointPlacement: 'on'
+        }
+      ]
     }
-  }
+  );
 
-  updateSeries() {
-    this.setState({
-      chartOptions: {
+  useEffect(() => {
+    if (botStats.randomBot !== undefined)
+      setChartOptions({
         series: [
           {
             name: 'Random Bot',
             data: [
-              this.props.botStats.randomBot.steps, 
-              this.props.botStats.randomBot.movement.rightDistance,
-              this.props.botStats.randomBot.tilesSeen,
-              this.props.botStats.randomBot.movement.leftDistance
+              botStats.randomBot.steps, 
+              botStats.randomBot.movement.rightDistance,
+              botStats.randomBot.tilesSeen,
+              botStats.randomBot.movement.leftDistance
             ],
             pointPlacement: 'on'
           },
           {
             name: 'Machine Learning Bot 1',
             data: [
-              this.props.botStats.mlBot1.steps, 
-              this.props.botStats.mlBot1.movement.rightDistance,
-              this.props.botStats.mlBot1.tilesSeen,
-              this.props.botStats.mlBot1.movement.leftDistance
+              botStats.mlBot1.steps, 
+              botStats.mlBot1.movement.rightDistance,
+              botStats.mlBot1.tilesSeen,
+              botStats.mlBot1.movement.leftDistance
             ],
             pointPlacement: 'on'
           },
           {
             name: 'Machine Learning Bot 2',
             data: [
-              this.props.botStats.mlBot2.steps, 
-              this.props.botStats.mlBot2.movement.rightDistance,
-              this.props.botStats.mlBot2.tilesSeen,
-              this.props.botStats.mlBot2.movement.leftDistance
+              botStats.mlBot2.steps, 
+              botStats.mlBot2.movement.rightDistance,
+              botStats.mlBot2.tilesSeen,
+              botStats.mlBot2.movement.leftDistance
             ],
             pointPlacement: 'on'
           }
         ]
-      }
-    })
-  }
+      });
+  }, [botStats]);
 
-  render() {
-    const { chartOptions } = this.state;
-
-    return (
-      <HighchartsReact 
-        highcharts={Highcharts}
-        options={chartOptions}
-      />
-    )
-  }
+  return (
+    <HighchartsReact 
+      highcharts={Highcharts}
+      options={chartOptions}
+    />
+  )
 }
 
 function mapStateToProps(state) {
